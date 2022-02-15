e5a8952222c343806cc0d1bd68533146a5780063
Hemant <basaltrock3@gmail.com>
2022-02-15 12:59:33 +0530

    :tada: First commit

---
+++ hemant.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, s, p, f;
+    a = 10;
+    b = 20;
+    c = 30;
+    s = a + b + c;
+    p = a * b * c;
+    f = s / p;
+    cout << "the sum of three integers a, b and c is "<<(e+1) ; 
+    return 0;
+}
